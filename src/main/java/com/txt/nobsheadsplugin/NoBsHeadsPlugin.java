1a03ad1ff4a9929ddbca52cad6aecacf79230d71
T-x-T <contact@thetxt.club>
2021-06-21 18:09:01 +0200

    Add scaffolding

---
+++ src/main/java/com/txt/nobsheadsplugin/NoBsHeadsPlugin.java
@@ -0,0 +1,19 @@
+package com.txt.nobsheadsplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NoBsHeadsPlugin extends JavaPlugin {
+  public NoBsHeadsPlugin() {
+
+  }
+
+  @Override
+  public void onEnable(){
+    
+  }
+
+  @Override
+  public void onDisable(){
+
+  }
+}
